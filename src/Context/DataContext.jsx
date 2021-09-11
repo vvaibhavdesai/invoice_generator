7776b3debc0b47d333f39aef6b397805fbb35f31
Vaibhav Desai <vaibhavdesai888@gmail.com>
2021-09-11 21:11:36 +0530

    added state for country form

--- src/Context/DataContext.jsx
+++ src/Context/DataContext.jsx
@@ -3,9 +3,32 @@ import { useState, useReducer, createContext, useContext } from "react";
 export const DataContext = createContext();
 
 export function DataContextProvider({ children }) {
-  const [modal, setModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [invoiceModal, setInvoiceModal] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [pincode, setPincode] = useState("");
   return (
-    <DataContext.Provider value={{ modal, setModal }}>
+    <DataContext.Provider
+      value={{
+        showModal,
+        setShowModal,
+        invoiceModal,
+        setInvoiceModal,
+        fullName,
+        setFullName,
+        phone,
+        setPhone,
+        address,
+        setAddress,
+        email,
+        setEmail,
+        pincode,
+        setPincode,
+      }}
+    >
       {children}
     </DataContext.Provider>
   );
