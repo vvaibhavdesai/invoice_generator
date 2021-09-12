98838bd5084dde78be3f3fc92b11033630523b7a
Vaibhav Desai <vaibhavdesai888@gmail.com>
2021-09-12 23:32:12 +0530

    added state variables  to context

--- src/Context/DataContext.jsx
+++ src/Context/DataContext.jsx
@@ -10,9 +10,28 @@ export function DataContextProvider({ children }) {
   const [address, setAddress] = useState("");
   const [email, setEmail] = useState("");
   const [pincode, setPincode] = useState("");
+  const [id, setId] = useState(null);
+  const [{ invoiceList }, dispatch] = useReducer(reducer, {
+    invoiceList: [],
+  });
+  function reducer(state, action) {
+    switch (action.type) {
+      case "ADD_ITEMS":
+        return {
+          ...state,
+          invoiceList: state.invoiceList.concat(action.payload),
+        };
+      default:
+        return state;
+    }
+  }
   return (
     <DataContext.Provider
       value={{
+        invoiceList,
+        dispatch,
+        id,
+        setId,
         showModal,
         setShowModal,
         invoiceModal,
