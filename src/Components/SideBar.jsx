284de8a547e9f97c356eb2d0fddd8f0c1b11753a
Vaibhav Desai <vaibhavdesai888@gmail.com>
2021-09-12 23:40:06 +0530

    updated sidebar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,25 +1,45 @@
+import { useDataContext } from "./../Context/DataContext";
+import { formatDate } from "./../Utils/dateformatter";
 export function SideBar() {
+  const { invoiceList, setId, id } = useDataContext();
+
   return (
     <aside className="sidebar">
       <section className="search-section">
         <input className="search-input" type="text" placeholder="search..." />
       </section>
       <ul className="sidebar-list">
-        <li>
-          <div className="invoice-list">
-            <div className="invoice-details">
-              <h4>Inv: #1</h4>
-              <time>xya asd</time>
-            </div>
-            <div className="invoice-details-bottom-row">
-              <div className="invoice-bottom-row-details">
-                <p>Items -1</p>
-                <p className="weight600 invoice-details-row-name"> Vaibhav Desai</p>
+        {invoiceList.length > 0 &&
+          invoiceList.map((item) => (
+            <li
+              className={id === item.id ? `selected` : ``}
+              onClick={() => setId(item.id)}
+              key={item.id}
+            >
+              <div className="invoice-list">
+                <div className="invoice-details">
+                  <h4 className={id === item.id ? `white` : ``}>
+                    Inv: #{item.id}
+                  </h4>
+                  <time>{formatDate(item.date)}</time>
+                </div>
+                <div className="invoice-details-bottom-row">
+                  <div className="invoice-bottom-row-details">
+                    <p className={id === item.id ? `white` : ``}>
+                      Items- {item.itemsList.length}
+                    </p>
+                    <p className="weight600 invoice-details-row-name">
+                      {" "}
+                      {item.customer.name}
+                    </p>
+                  </div>
+                  <p className={`${id === item.id ? `white` : ``} weight600`}>
+                    ₹{item.grandTotal}
+                  </p>
+                </div>
               </div>
-              <p className="weight600">₹2000</p>
-            </div>
-          </div>
-        </li>
+            </li>
+          ))}
       </ul>
     </aside>
   );
