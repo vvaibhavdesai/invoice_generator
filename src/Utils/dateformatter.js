36f318a564a87c21bd0580e3784de0bd49ae385a
vvaibhav <vaibhavdesai888@gmail.com>
2021-12-27 01:10:00 +0530

    removed

--- src/Utils/dateformatter.js
+++
@@ -1,5 +0,0 @@
-export  function formatDate(date){
-    const newDate = new Date(date).toLocaleDateString()
-    const newTime = new Date(date).toLocaleTimeString()
-    return `${newDate} ${newTime}`
-  }
