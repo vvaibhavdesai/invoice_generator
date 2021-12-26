04557e2c9f515e43ed673833086588fba586b0da
vvaibhav <vaibhavdesai888@gmail.com>
2021-12-27 01:03:13 +0530

    refactored navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import plus from "../assets/plus.png";
+import { useDataContext } from "../Context/DataContext";
+import Button from "../utils/Button";
+
+export function Navbar() {
+  const { setShowModal } = useDataContext();
+  return (
+    <nav className="weight600 header-color">
+      <p>DashBoard</p>
+      <Button
+        callback={setShowModal}
+        callbackArgs={true}
+        image={plus}
+        styleClass={"add-invoice-btn make-center"}
+        imageStyleClass={"plus-img"}
+      />
+    </nav>
+  );
+}
